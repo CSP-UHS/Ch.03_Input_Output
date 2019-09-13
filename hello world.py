9df27d8de476836209637fab9494fbbe2ed9ed32
cobbzac <cobbzac@urbandaleschools.com>
2019-09-13 14:24:09 -0500

    sick

--- hello world.py
+++ hello world.py
@@ -4,6 +4,7 @@ print(4+5)
 print("4+5")
 score = 120
 print("your score is:",score)
-print("I want to print a double quote." \n right here")
+print("I want to print a double quote. \n right now")
 
-
+firstname =  input("what is is your first name")
+print
