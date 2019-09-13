2a9d174c7d7909944a471a9701273d1516643110
barleypeg <barleypeg@urbandaleschools.com>
2019-09-13 14:26:36 -0500

    merging into the master
    # Conflicts:
    #	1.1_Github_VCS.py

---
+++ Helloworld.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+print("This is me!")
+print(5+4)
+print("5+4")
+score = 140
+score+=1
+print("Your score is", score)
+print("I want to print a double quote \nBut I won't right now" )
+
+# highlight, command / to comment
+# highlight, tab to tab over, highlight, shift tab to un tab over
+#look up \o and \n
+
+first_name= input("What is your first name?")
+last_name= input("What is your last name?")
+print("Nice to meet you",first_name,last_name)
