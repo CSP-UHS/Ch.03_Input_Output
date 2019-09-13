d3f7e009c2fbb0b3caae4683e1d77a914b93a4b8
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-13 14:23:16 -0500

    Name

---
+++ Hello_World.py
@@ -0,0 +1,17 @@
+#tells you your age
+print("Hey let me guess what is your age is.")
+
+BornY = int(input())
+
+print(2019 - BornY)
+
+# print("Hey wats your grade \" Hello") HOW TO PRINT A DOUBLE QUOTE
+# \n = new line
+
+#let me guess your grade!
+
+# print("let me guess your grade, type in when you graduate")
+#
+# gy = int(input())
+#
+# print(12-( gy - 2019))
