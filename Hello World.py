b1a25f864c7190b2f8cceb1363698f799249761d
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-16 14:23:28 -0500

    new code

--- Hello World.py
+++ Hello World.py
@@ -2,5 +2,11 @@ print("Hellow World")
 print("this is me!")
 print(5+4)
 print("5,4")
-score+1 = 180
-print("your score is:"score)
+score= 180
+score+=1
+print("your score is:",score)
+print("i want this to be a double quote. \n but I wont Right Now")
+Length=int("what is the length of the rectangle")
+Width=int(""What is the width of the rectangle")
+Area=Length*Width
+print("The area of the rectangle is",area)
