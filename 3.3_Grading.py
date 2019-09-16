8cb28df90b1c28177cb6ed55cc4d9c6803b25225
hermoniuscapensis <phamjul@urbandaleschools.com>
2019-09-16 15:58:14 -0500

    DONE =D

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,6 +12,10 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
-Sem_Grade=int(input("What is the semester grade?"))
-Final_Exam=int(input("What is the final exam?"))
-Exam_Worth=int(input("What is the exam worth?"))
+Sem_Grade=float(input("What is the semester grade?"))
+Final_Exam=float(input("What is the final exam?"))
+Exam_Worth=float(input("What is the exam worth?"))
+Exam_Worth=Exam_Worth/100
+x=1-Exam_Worth
+Final_Grade=(Sem_Grade* x)+(Final_Exam*Exam_Worth)
+print(Final_Grade)
