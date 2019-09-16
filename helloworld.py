925b4a3b778461fd5ff2a741bd965aa839394240
buiste <buiste@urbandaleschools.com>
2019-09-16 13:48:40 -0500

    commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+print("This is me!")
+print(5+4)
+
+print("5+4")
+score = 128
+score+=1
+print("Your score is",score)
+print("I want to print a double quote. \n But I won't right now!")
+firstname = input("What is your first name?")
+lastname = input("What is your last name?")
+print("Nice to meet you,",firstname,lastname)
