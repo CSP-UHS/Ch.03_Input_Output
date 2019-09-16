4a8c82f9443d325f1653f73de05247fabc8da2a4
hmarmal <hmarmal@urbandaleschools.com>
2019-09-16 13:48:02 -0500

    This is the one

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("this is me")
+print(5+4)
+print("5+4")
+score = 120
+print ("Your score is:", score)
+print(" I want to print a double quote \\ right here")
+
+firstname = input("what is your first name?")
+print(firstname +" you are gay")
