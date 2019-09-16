a2be0cf4c6e047b778da5099f9e76bd18af07bae
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-16 13:57:23 -0500

    Just yep

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -3,36 +3,39 @@
 
 # 1.) Write a program that asks someone for their name and then prints their name to the screen?
 
-    # print('Hello What is your Name')
-    #
-    # name = input("What's your name?")
-    #
-    # print(" Hello," + name + "!")
+# print('Hello What is your Name')
+#
+# name = input("What's your name?")
+#
+# print(" Hello," + name + "!")
 
 
 # 2. Write a a program where a user enters a base and height and you print the area of a triangle.
 
-print("Hey give me a base and a hight")
+# print("Hey give me a base and a hight")
+#
+# base = int(input("Enter base"))
+#
+# height = int(input("Enter height"))
+#
+# area = (base*height*0.5)
+#
+# print(area)
 
-base = int(input("Enter base"))
-
-height = int(input("Enter height"))
-
-print(base * height * 0.5)
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
-    # print("Hello what is a radius of a circle?")
-    #
-    # radius = int(input()) #this will change the string into a int
-    #
-    # print (2 * 3.14 * radius)
+# print("Hello what is a radius of a circle?")
+#
+# radius = int(input()) #this will change the string into a int
+#
+# print (2 * 3.14 * radius)
 
 
 # 4. Ask a user for an integer and then print the square root.
 
-    # integer = int(input('Hello! give me an integer!'))
-    #
-    # print(integer**.5)
+# integer = int(input('Hello! give me an integer!'))
+#
+# print(integer**.5)
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma.
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
