09149f776278a0b608ad4ab89326e53ca4a31a8a
watsoncal <watsoncal@urbandaleschools.com>
2019-09-17 14:17:41 -0500

    Initial Code

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,11 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+print("Hi, I hope you are not too worried about your overall grades!\nHere's something to help, though.")
+sem_grade=int(input("What is your semester grade? "))
+fin_grade=int(input("What is your final exam grade? "))
+fin_worth=float(input("What is your final exam worth written as a decimal? "))
+sem_worth=1-fin_worth
+ovr_grade=(sem_grade*sem_worth)+(fin_grade*fin_worth)
+print("Your overall grade is",ovr_grade)
