c0e918fa7930e829dcb9508cb6bfee033505c45b
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-17 14:10:31 -0500

    new code

---
+++ Jedi_Training_3.0.py
@@ -0,0 +1,8 @@
+input("what is your First and Last Name?")
+TriangleBase=int(input("what is the base of your triangle?"))
+TriangleHeight=int(input("what is the height of your triangle"))
+Area=(TriangleBase*TriangleHeight)/2
+print("the very cool and epic area of your triangle is",Area)
+CircleRadius=int(input("what is the radius of your circle"))
+CircleCir=(CircleRadius*2)*3.14
+print("the circumference of your cirlce is",CircleCir)
