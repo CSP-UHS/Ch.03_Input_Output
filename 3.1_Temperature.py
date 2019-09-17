6c0efc26ae3d3198a2a6fe540d01fc3041f303b1
watsoncal <watsoncal@urbandaleschools.com>
2019-09-17 14:00:15 -0500

    Converted Fahrenheit into Celsius Temperature

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,15 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ???
+In: -40  Out: -40
 
 '''
 
+print("Welcome to my temperature program!")
+temp_f=int(input("Please enter a temperature in Fahrenheit. "))
+print("This temperature will now be converted into Celsius.")
+temp_c=(temp_f-32)*(5/9)
+print("The temperature in Celsius is",temp_c)
 
 
 
