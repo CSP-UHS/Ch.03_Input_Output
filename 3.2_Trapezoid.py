492cb237f854235f1a9a8111484da4efd2655fc8
watsoncal <watsoncal@urbandaleschools.com>
2019-09-17 14:06:56 -0500

    Calulated the Area of a Trapezoid

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,11 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Welcome to my trapezoid program!\nWant an easy way to calculate the area of a trapezoid?")
+base_1=int(input("Please enter the first base. "))
+base_2=int(input("Please enter the second base. "))
+height=int(input("Please enter the height. "))
+print("The area of the trapezoid will now be calculated!")
+area=(base_1+base_2)*(height/2)
+print("The area is",area)
