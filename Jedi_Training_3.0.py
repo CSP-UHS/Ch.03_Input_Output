5f69541cc5d6e2fb1ad4a4dff8eb13ad5efea705
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-17 14:25:05 -0500

    finished code

--- Jedi_Training_3.0.py
+++ Jedi_Training_3.0.py
@@ -1,4 +1,5 @@
-input("what is your First and Last Name?")
+Name=input("what is your First and Last Name?")
+print("Yo wassup",Name)
 TriangleBase=int(input("what is the base of your triangle?"))
 TriangleHeight=int(input("what is the height of your triangle"))
 Area=(TriangleBase*TriangleHeight)/2
@@ -6,3 +7,12 @@ print("the very cool and epic area of your triangle is",Area)
 CircleRadius=int(input("what is the radius of your circle"))
 CircleCir=(CircleRadius*2)*3.14
 print("the circumference of your cirlce is",CircleCir)
+FavInt=int(input("what is your favorite Integer?"))
+SquareRoot=FavInt**0.5
+print("The Square Root of your favorite interger is",SquareRoot)
+print("May the mass times acceleration be with you!")
+Mass=int(input("what is the mass you want?"))
+Acceleration=int(input("what's the acceleration you want?"))
+Force=Mass*Acceleration
+print("your Force is",Force)
+print("get it? hehe")
