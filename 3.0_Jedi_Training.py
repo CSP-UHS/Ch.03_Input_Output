8ee611728287d0b574b30902f1e3a9522bbf5976
smithdyl <smithdyl@urbandaleschools.com>
2019-09-19 13:55:44 -0500

    Training

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,20 +1,27 @@
-#Sign your name:________________
+#Sign your name:_Dylan Smith
 
 
 # 1.) Write a program that asks someone for their name and then prints their name to the screen?
-
+#FN=input("What is your Full Name?")
+#print("Hello",FN)
 
 # 2. Write a a program where a user enters a base and height and you print the area of a triangle.
-
+#Ht=int(input("What is the height of the triangle?"))
+#Bs=int(input("What is the base of the triangle?"))
+#Area=print("The area of your triangle is", (Ht*Bs)/2)
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
-
+#r=int(input("What is the radius of the circle?"))
+#print("The circumference of your circle is", (2*r)*3.14)
 # 4. Ask a user for an integer and then print the square root.
-
-
-# 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
+#Nb=int(input("What integer would you like the square root of?"))
+#print("The square root of your integer is", Nb**.5)
+# 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma.
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
+Ma=int(input("What is the mass of your object?"))
+Ac=int(input("What is the objects acceleration?"))
+print("The force of the object is", Ma*Ac,", A sith you are hrmmmmm")
+print("Get it?")
 
 
 
