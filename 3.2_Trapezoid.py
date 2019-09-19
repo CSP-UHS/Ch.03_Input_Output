8b7892120eaa0337f071468a74a15d7a8d957ed8
hmarmal <hmarmal@urbandaleschools.com>
2019-09-19 13:40:05 -0500

    Grading

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,10 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Give me information to calculate the area of a trapezoid")
+b1=int(input("base # 1"))
+b2=int(input("base # 2"))
+h=int(input("height"))
+area= (b1+b2)/2*h
+print("The are of the Trapezoid is ",area)
--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,11 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+print("Hey! calculate your Overall exam score!!")
+S=int(input("Sem Grade:"))
+F=int(input("Final Exam:"))
+E=int(input("Exam worth in %:"))
+E=E/100
+overal=S*(1-E)+F*(E)
+print(overal)
