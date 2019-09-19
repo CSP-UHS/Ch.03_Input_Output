7e7ac2453b7c3716152e87574779f1d28b489cd6
smithdyl <smithdyl@urbandaleschools.com>
2019-09-19 14:15:37 -0500

    Trapezoid

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,10 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Welcome to Smith's Trapezoid Area Calculator")
+b1=int(input("What is the First Base of the trapezoid?"))
+b2=int(input("What is the Second Base of the Trapezoid?"))
+h=int(input("What is the Height of the Trapezoid?"))
+print("The area of your trapezoid is.......",((b1+b2)/2)*h )
+print("Thank you, please come again")
