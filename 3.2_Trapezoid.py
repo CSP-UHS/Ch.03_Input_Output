9c73466ed833ccb0db81f035288fa2ce0178e3dc
buiste <buiste@urbandaleschools.com>
2019-09-19 13:40:38 -0500

    commit

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,10 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Welcome to the Trapizoid Area Calculator")
+base1=int(input("What is the base 1?"))
+base2=int(input("What is the base 2?"))
+height=int(input("What is the height?"))
+area=(base1+base2)/2*height
+print("Your Trapezoid area is",area)
--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,10 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+print("Welcome to the Grading Calculator!")
+SG=int(input("What is your semester grade?"))
+FE=int(input("What is your final exam grade?"))
+EW=int(input("What is the exam worth?"))
+EW=EW/100
+overall=SG*(1-EW)+FE*(EW)
+print("This is your Final Grade",overall)
