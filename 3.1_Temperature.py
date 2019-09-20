e56bb2a54068b06ea76f6f6fcd9821863650e5ac
smithdyl <smithdyl@urbandaleschools.com>
2019-09-20 13:50:16 -0500

    Temp

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -15,7 +15,7 @@ In: -40  Out: -40
 
 print("Hello welcome to Smith's Fahrenheit to Celsius Converter")
 F=int(input("What is the Fahrenheit you would like to convert?"))
-print("The celsius is.......",((F-32)*5)/9 )
+print("The celsius is.......",((F-32)*5)/9)
 print("Thank you, please come again")
 
 
