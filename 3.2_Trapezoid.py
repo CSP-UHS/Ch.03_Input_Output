8d2647c84d568eda9cbf3a592e76c257879cabe4
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-23 14:00:09 -0500

    This is for the trapezoid program

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,8 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+Base1=int(input("what is the first base?"))
+Base2=int(input("what is the second base?"))
+Height=int(input("what is the height of the trapezoid?"))
+Area=(Base1+Base2)/2*Height
+print("Your area is",Area)
