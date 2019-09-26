e197c0fdab7953568913c70ade1ee0af60bb40ed
cobbzac <cobbzac@urbandaleschools.com>
2019-09-26 13:40:00 -0500

    finished ch 2

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,7 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+S = int(input("enter semester grade"))
+F = int(input("enter final exam percentage"))
+W = int(input("enter the weight of the final exam"))
