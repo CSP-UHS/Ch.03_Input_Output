921549777b2eda024b04638d3c36cd7dabf9514d
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-09-18 10:32:54 -0500

    mpg

---
+++ pratice.py
@@ -0,0 +1,5 @@
+print("WELCOME, to the MPG calculator\n")
+miles=float(input("Please enter miles traveled: "))
+gal=float(input("How many gallons of gas did you use: "))
+mpg=miles/gal
+print("Your gas mileage is",mpg,"mpg")
