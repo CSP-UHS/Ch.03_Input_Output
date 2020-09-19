5068090f3d2e8d90415f46031ee6cb9c53e501ae
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-09-18 20:24:16 -0500

    commit1

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,11 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+print("Want to know the area of a trapezoid?\n\nPut the figures below to find out the area\n")
+bsa=float(input("Base A: "))
+bsb=float(input("Base B: "))
+hgt=float(input("Height: "))
+area=bsa+bsb
+area=area/2
+area=area*hgt
+print("\nThe area of the trapezoid is:",area)
