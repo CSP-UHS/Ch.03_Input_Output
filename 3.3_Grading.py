cf178209e44c41abc38bf51cc51be3efaf1c3713
caltrideraar <caltrideraar@urbandaleschools.com>
2020-09-24 10:27:36 -0500

    Finished

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,11 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+og = int(input("Enter your current grade: "))
+fg = int(input("Enter the final grade: "))
+worth = int(input("Enter the final worth: "))
+worthperc = worth/100
+ogperc = 1-worthperc
+total = (og*ogperc) + (fg*worthperc)
+print("Your grade is:" ,total,end='')
