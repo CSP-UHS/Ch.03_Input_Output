140b4840899858dfc79f999473dc1e8eb778eaac
caltrideraar <caltrideraar@urbandaleschools.com>
2020-09-24 10:18:11 -0500

    Finished Trapezoid

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,9 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+base = int(input("Enter the base: "))
+base2 = int(input("Enter the second base: "))
+height = int(input("Enter the height: "))
+area = (base+base2) / 2 * height
+print("Your area is:" ,area,end='')
