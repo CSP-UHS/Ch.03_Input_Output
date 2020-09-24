d19e1932acb60c4466dddd3b5b151f7e6d1cb776
hmarlal1 <hmarlal1@urbandaleschools.com>
2020-09-24 10:20:44 -0500

    did the grading thing

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,10 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+sem_Grade = int(input("Enter your semester grade:"))
+final_Exam = int(input("Enter your score for your final exam:"))
+exam_Worth = int(input("Enter the % of your grade the exam is worth:"))
+exam_Worth: float = exam_Worth/100
+overall_Grade = sem_Grade*(1-exam_Worth)+final_Exam*exam_Worth
+print('This is your overall grade:', overall_Grade)
