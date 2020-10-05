3bb04f119c812cc1bf09ffe4a68f7331d39ea3c7
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-10-05 10:01:35 -0500

    commitanswer

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,14 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+examgrade=float(input("What is your Final exam grade?"))
+examworth=float(input("How much is the Final worth?"))
+semgrade=float(input("What is your Semester grade?"))
+examworth=examworth/100
+examworthe=1-examworth
+examgrade=examgrade*examworth
+semgrade=semgrade*examworthe
+overall=examgrade+semgrade
+print()
+print("Here's your overall grade:")
+print(overall)
