ddc474a0c00e62e15af48bece744aa3b68651a71
redeniuseva <redeniuseva@urbandaleschools.com>
2020-10-16 10:00:07 -0500

    Finished Quiz

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -3,11 +3,13 @@
 
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-
-
+PER=(input("What is your name? "))
+print("Hey there" ,PER,)
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
-
+b=int(input("What is the base of the triangle"))
+h=int(input("What is the height of the triangle"))
+A= (b*h)/2
+print("The area is",A)
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
 
