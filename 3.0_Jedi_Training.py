7c941978b2f6a98f334b2a014f0477f287b4f14f
redeniuseva <redeniuseva@urbandaleschools.com>
2020-10-21 09:26:53 -0500

    Finished Jedi trainings

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,6 +1,5 @@
-# Sign your name:________________
+# Sign your name:Evan Redenius
 # In all the short programs below, do a good job communicating with your end user!
-
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 PER=(input("What is your name? "))
@@ -10,14 +9,21 @@ b=int(input("What is the base of the triangle"))
 h=int(input("What is the height of the triangle"))
 A= (b*h)/2
 print("The area is",A)
+
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
-
+R=int(input("What is the radius of your circle: "))
+C=(R*3.14)*2
+print("The circumfrince is",C)
 # 4. Ask a user for an integer and then print the square root.
-
-
+import math
+i=int(input("What number do you want the square root of:"))
+SQ=math.sqrt(i)
+print("This is the square root",SQ)
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
 
-
-
+m = int(input("What is the mass: "))
+a = int(input("What is the acceleration: "))
+F = m*a
+print(F)
+print("get it?")
