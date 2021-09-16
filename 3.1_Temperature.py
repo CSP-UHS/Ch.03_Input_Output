397a3a0d61cae27bf2bd37c84ce27ee6e0025395
swartzaid <swartzaid@urbandaleschools.com>
2021-09-16 10:05:07 -0500

    temperature commit

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,11 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: -40.0
 
 '''
-
-
+F=float(input("what is the temperature?"))
+C=(F-32)*(5/9)
+print(C)
 
 
