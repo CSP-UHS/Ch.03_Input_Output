849e8cbf4bc8f0c39e833784d5c02a53ea082afb
toricden <toricden@urbandaleschools.com>
2021-09-16 10:28:59 -0500

    Save Cinco

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,8 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+base= int(input("What's the base of the trapezoid?"))
+base2= int(input("What's the second or other base of the trapezoid?"))
+height= int(input("What's the height of the trapezoid"))
+area= (base+base2)/2*height
+print("The area of your trapezoid is",area,)
