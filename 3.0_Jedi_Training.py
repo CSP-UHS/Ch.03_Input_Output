f091e75cca7006f55bf4fbe50b3986acbde32ffd
swartzaid <swartzaid@urbandaleschools.com>
2021-09-16 09:39:25 -0500

    python console commit

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,21 +1,32 @@
 # Sign your name:________________
 # In all the short programs below, do a good job communicating with your end user!
-
+import math
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-
+name=str(input("what is your name?"))
+print("Hello", name)
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
-
+base=float(input("what is the base?"))
+height=float(input("what is the height?"))
+area= base*height
+print(area)
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
-
+radius=float(input("what is the radius?"))
+circ=(radius*2*math.pi)
+print(circ)
 # 4. Ask a user for an integer and then print the square root.
-
+Number= float(input("what is the number?"))
+root=math.sqrt(Number)
+print(root)
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-
+print("May the mass times acceleration be with you!")
+mass=float(input("what is the mass?"))
+acc=float(input("what is the acceleration"))
+force=(mass*acc)
+print(force)
+print("Get it?")
 
 
---
+++ Hello World.py
@@ -0,0 +1,5 @@
+# gas mileage program
+miles_driven = float(input("how long was your trip? "))
+gallons_used = float(input("How many gallons of gas did you use? "))
+mpg = miles_driven/gallons_used
+print("Your gas mileage was",mpg,"mpg")
