b396509561bf7ff7427f2815e3bb3b0ed0de9f8f
clarksongav <ClarksonGav@urbandaleschools.com>
2021-09-23 10:15:48 -0500

    dingus

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# Gas mileage program
+
+miles_driven = float(input("How long was your trip?"))
+gallons_used = float(input("How many gallons of gas was used"))
+mpg=miles_driven/gallons_used
+print("Your gas mileage was" ,mpg,"mpg")
+
+
+
