7f93ea8b854c2bc0ed42e9e90a04d5fadcb95733
evittstan <evittstan@urbandaleschools.com>
2022-09-16 08:06:12 -0500

    Question 1 answer

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -4,8 +4,12 @@
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 
+name = input("What's your name?")
+Greeting = ("Hello, " + name + " nice to meet you")
+print(Greeting)
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
+
 
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
@@ -16,6 +20,4 @@
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-
-
 
