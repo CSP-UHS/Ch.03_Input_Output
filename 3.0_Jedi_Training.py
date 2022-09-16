84d8fc05d3e19817f4913a1ccde0c7bcbdd1c25d
evittstan <evittstan@urbandaleschools.com>
2022-09-16 08:19:14 -0500

    Question 2 answer

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -4,15 +4,19 @@
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 
-name = input("What's your name?")
-Greeting = ("Hello, " + name + " nice to meet you")
-print(Greeting)
+#name = input("What's your name?")
+#Greeting = ("Hello, " + name + " nice to meet you")
+#print(Greeting)
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
 
-
+base = int(input("What is the base of the triangle"))
+height = int(input("What is the height of the triangle"))
+areaOfTriangle = base*height/2
+print("The area of the Triangle is:",areaOfTriangle,"cm^2")
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
+
 
 
 # 4. Ask a user for an integer and then print the square root.
