fb8f9edb93cb749ec9c72ccc752472e5786f12ec
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-16 08:56:24 -0500

    temp done

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,12 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: ??? Please tell me what this output is! -40
 
 '''
 
-
+fah = float(input("What is you temp in fahrenheit?"))
+cel = (fah-32)*(5/9)
+print("Your temp in celsius is" ,cel, "that's hot.")
 
 
