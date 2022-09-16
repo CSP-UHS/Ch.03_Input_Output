e5f6834f2751999d0c29bb80b35bf3aa58bb6e7f
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-16 11:45:40 -0500

    Done with all

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -16,7 +16,7 @@ Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 print("Hey I know you suck at your classes but everything will be ok. Let's take a look at your grades.")
 semGrade = float(input("What is your grade for this semester?"))
 finExam = float(input("What did you get on your final exam?"))
-examWorth = float(input("How much was the final worth"))
+examWorth = float(input("How much was the final worth?"))
 overall = (finExam*(examWorth/100))+(semGrade*((100-examWorth)/100))
 
 print("Your final grade for the year is" ,overall,"%. Good Job")
