e4d144c2a7d907a7dcc5cdb608f2baa2014d29c7
huynheth <huynheth@urbandaleschools.com>
2022-09-16 08:26:45 -0500

    sept 16

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,15 +1,24 @@
-# Sign your name:________________
+# Sign your name: Ethan Huynh
 # In all the short programs below, do a good job communicating with your end user!
 
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 
+# nm=input("what is your name?")
+# print("hello "+nm+" how are you doing today")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
 
+# bs=float(input("what is the base of your triangle in centimeters?"))
+# ht=float(input("what is the height of your triangle in centimeters"))
+# area=bs*ht/2
+# print("the area of the triangle was",area,"cm squared!")
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
+# rad=float(input("what is the radius of your circle?"))
+# circum=2*3.14*rad
+# print("the circumference of your circle is",circum,"squared")
 
 # 4. Ask a user for an integer and then print the square root.
 
@@ -17,10 +26,5 @@
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
 
-
-md=int(input("enter the miles driven: "))
-gu=int(input("enter the gallons used: "))
-mpg=(md/gu)
-print(mpg)
 
 
