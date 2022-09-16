e1e38e9f18a2522c50b14ffa26bff00f5c686aec
learari <learari@urbandaleschools.com>
2022-09-16 08:43:50 -0500

    Final Commit

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,21 +1,38 @@
-# Sign your name:________________
+# Sign your name: Arianna Lear
 # In all the short programs below, do a good job communicating with your end user!
-
+import math
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-
+# username = input("Hello! What is your name? ")
+# print("Hello, ", username,", how are you doing today?")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
+# base = float(input("What is the base of your triangle? "))
+# height = float(input("What is the height of your triangle? "))
+# area = (base*height)/2
+# print("The area of the triangle was ",area," cm^2)")
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
+# r = float(input("What is the radius of the circle? "))
+# c = (2*math.pi*r)
+# print("The circumference of your circle is ",c," meters.")
 
 # 4. Ask a user for an integer and then print the square root.
-
+# n = float(input("What number did you want to square root? "))
+# sr = math.sqrt(n)
+# print("The square root of ",n," is ",sr,".")
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
+mass = float(input("What is the mass? "))
+acc = float(input("What is the acceleration? "))
+force = mass*acc
+print("May the ",mass*acc," be with you!")
+print("Get it?")
+# print("I want to print a double quote "\"right here")
 
-
-
+'''
+This is a docstring
+ '''
+# highlight code the command+/ to put all highlighted code in #'s
+# command+z brings back deleted code
