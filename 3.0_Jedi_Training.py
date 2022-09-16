0965765dfb83f8a483aae99f6ccecd70c1efb596
evittstan <evittstan@urbandaleschools.com>
2022-09-16 08:24:59 -0500

    Question 3 answer

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -10,16 +10,19 @@
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
 
-base = int(input("What is the base of the triangle"))
-height = int(input("What is the height of the triangle"))
-areaOfTriangle = base*height/2
-print("The area of the Triangle is:",areaOfTriangle,"cm^2")
+#base = int(input("What is the base of the triangle?"))
+#height = int(input("What is the height of the triangle?"))
+#areaOfTriangle = base*height/2
+#print("The area of the Triangle is:",areaOfTriangle,"cm^2")
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
-
+#radius = float(input("What is the radius of the circle in meters?"))
+#circumference = 2*3.14*radius
+#print("The circumference of your circle is",circumference,"meters")
 
 # 4. Ask a user for an integer and then print the square root.
+
 
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
