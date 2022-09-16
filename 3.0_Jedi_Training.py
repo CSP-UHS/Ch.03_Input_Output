a80ff7bdf64470c4a6e3c9f51d2244edaad11490
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-16 08:44:22 -0500

    Jedi Training done

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -3,21 +3,29 @@
 
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-
+# username = input("Hello! What is your name?")
+# print("Hello "+username+ ", why do you cry.")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
-
+# base = float(input("What is the base of your triangle?"))
+# height = float(input("What is the height of your triangle?"))
+# area = base*height/2
+# print("The area of the triangle is" ,area, "yay math")
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
+import math
+# radius = float(input("What is the radius of your circle?"))
+# circ = 2*radius*math.pi
+# print("The circumference of your circle is" ,circ, "now get a calculator you idiot.")
 
 # 4. Ask a user for an integer and then print the square root.
-
-
+# num = int(input("Enter an integer:"))
+# sqRt = math.sqrt(num)
+# print("The square root of" ,num,"is" ,sqRt,)
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
-#    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-
-
+# Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
+# mass = float(input("What is the mass of the object?"))
+# acc = float(input("What is the acceleration of the object?"))
+# print("May the" ,mass*acc, "be with you. Get it. It's the force!")
 # print("Hello World")
 #
 # print("Your new score is", 10123+13813, "pts")
@@ -32,7 +40,7 @@
 this is a docstring
 '''
 
-milesDriven = float(input("Enter the mile driven:"))
-gallonsUsed = float(input("Enter the gallons driven:"))
-mpg = milesDriven/gallonsUsed
-print(mpg)
+# milesDriven = float(input("Enter the mile driven:"))
+# gallonsUsed = float(input("Enter the gallons driven:"))
+# mpg = milesDriven/gallonsUsed
+# print(mpg)
