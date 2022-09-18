107d6352073f2fb1716f54bab43ba9668213e64b
zimmermanaid <zimmermanaid@urbandaleschools.com>
2022-09-18 14:07:15 -0500

    ch3trap done

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,9 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Area of trapezoid")
+a=float(input("What is the length of the top side:"))
+b=float(input("What is the length of the bottom side:"))
+h=float(input("What is the height:"))
+print("The area of this trapezoid is:",(a+b)/2*h)
