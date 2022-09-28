4750c99d1a42b42cab376dd6099940916ffd8cab
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-09-28 08:53:57 -0500

    sem finished

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -14,7 +14,12 @@ Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
 semester = float(input("What is your semester grade?"))
 final = float(input("What was your final grade in the class?"))
-weight = float(input("What is the weight of the test?"))
-over = (semester*final)/(100/weight)/3
+tw = float(input("What is the weight of the test?"))
+tw=tw/100
+gw=1-tw
+ave = gw*semester + tw*final
 
-print("Your over grade was",over,"for the class!")
+# over = semester+final+tw/3
+
+
+print("Your overall grade was",ave,"for the class!")
