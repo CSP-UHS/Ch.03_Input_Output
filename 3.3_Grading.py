de6c937bc9aaddde930376a04df8b577ddcbc5c9
evittstan <evittstan@urbandaleschools.com>
2022-09-28 08:07:28 -0500

    Grading code

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,10 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+semesterGrade = float(input("What was your semester grade?"))
+finalExam = float(input("What did you get on your final exam?"))
+examWorth = float(input("What was your final exam worth?"))/100
+semesterWorth = 1-examWorth
+overall = semesterWorth*semesterGrade+finalExam*examWorth
+print(overall)
