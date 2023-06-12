1020f702d9daec1998ba57a0dab09d7fadb7d295
Shea <hermon.oshea@carlislecsd.org>
2023-06-12 10:11:25 -0500

    finished

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,7 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+base1= float(input("Enter the 1st base:"))
+base2= float(input("Enter the 2nd base:"))
+height= float(input("Enter the Height:"))
+print("The area of your Trapezoid is:", (base1+base2)/2*height)
